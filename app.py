2beeba162e75551cc7e1105f337c8ae21da8a57f
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-16 11:50:17 +0000

    second flask run

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 # create an instance of our app
 app = Flask(__name__)
@@ -16,10 +16,16 @@ students = [
 # decorator - to create our api/url for user to access our data in the browser
 @app.route("/") # localhost:5000 is the default port for flask
 def home():
-    return "Consultant team"
+    return "<h1>Consultant team</h1>"
 # This function runs when the API/URL is accessed
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 
+# creating our own API to display data on the specific route/url/end-point
+@app.route("/api/v1/student/data", methods=["GET"])
+# this will add this API/URL to http://127.0.0.1:5000/api/v1/student/data
+def customised_api():
+    return jsonify(students) # transforms data into json
+    # Using Extract Transform Load
